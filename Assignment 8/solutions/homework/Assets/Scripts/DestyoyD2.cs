659fbae3c4c3b304bf4584747837d5c759441af9
Or Abuhtzura <44296674+orabu103@users.noreply.github.com>
2020-05-17 15:20:45 +0300

    Add AngryBirds

---
+++ Assignment 8/solutions/homework/Assets/Scripts/DestyoyD2.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestyoyD2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] float health = 8f;
+    public static int EnemyAlive = 0;
+
+    void Start()
+    {
+        EnemyAlive++;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log(collision.relativeVelocity.magnitude);
+        if (collision.relativeVelocity.magnitude > health)
+        {
+            Destroy(this.gameObject);
+            EnemyAlive--;
+            if (EnemyAlive <= 0)
+                Debug.Log("Level Up");
+        }
+    }
+}
