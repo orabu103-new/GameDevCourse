4051ffb9dabbe9384ac105f7e573a1f788c47772
Avihuoshri <avihust@gmail.com>
2020-05-20 02:13:29 +0300

    Title was added to main menu , and also sound to all 4 levels

--- Assignment 8/solutions/homework/Assets/AudioManager.cs
+++ Assignment 8/solutions/homework/Assets/AudioManager.cs
@@ -5,22 +5,42 @@ public class AudioManager : MonoBehaviour
 {
 
     public Sound[] sounds;
-    // Start is called before the first frame update
+
+    public static AudioManager instance;
     void Awake()
     {
+        if (instance == null)
+            instance = this;
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+        DontDestroyOnLoad(gameObject);
         foreach(Sound s in sounds)
         {
            s.source =  gameObject.AddComponent<AudioSource>();
             s.source.clip = s.clip;
             
             s.source.volume = s.volume ;
-            s.source.pitch = s.pitch ;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
         }
     }
 
+    void Start()
+    {
+        play("ThemeSong");
+    }
     public void play (string name)
     {
         Sound s = Array.Find(sounds, sound => sound.name == name);  //Shorter coding way to find element inside the array
+        if (s == null)
+        {
+            Debug.LogWarning("Sound: " + name + " not found!");
+            return;
+        }
+           
         s.source.Play();
     
     
--- Assignment 8/solutions/homework/Assets/Scripts/Sound.cs
+++ Assignment 8/solutions/homework/Assets/Scripts/Sound.cs
@@ -8,11 +8,12 @@ public class Sound
 
     public AudioClip clip;
 
-    [Range( 0f , 1f)]    //Volume slider
+    [Range( 0f , 3f)]    //Volume slider
     public float volume;
     [Range(1f , 3f)]     //Pitch slider
     public float pitch ;
 
+    public bool loop;
     [HideInInspector]
     public AudioSource source ;
 }
