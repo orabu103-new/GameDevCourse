746772aa8714c4a10c83d2a938c8e9d74e22a436
Avihuoshri <avihust@gmail.com>
2020-05-26 03:58:07 +0300

    Road manager script were added

---
+++ Assignment 9/solutions/yourgame/CanYouDrive1/Assets/Scripts/RoadGeneration.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadGeneration : MonoBehaviour
+{
+    [SerializeField]
+    GameObject[] gameObjects;
+    // Start is called before the first frame update
+    void Start()
+    {
+        int rand = Random.Range(0, gameObjects.Length);
+        Instantiate(gameObjects[rand], transform.position , Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assignment 9/solutions/yourgame/CanYouDrive1/Assets/Scripts/RoadManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadManager : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject[] roadPrefabs;
+    public float zspawn = 0;
+    public float roadLength = 30 ;
+    public int numOfRoads = 4; 
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0; i < numOfRoads; i++)
+        {
+            SpawnRoad(Random.Range(0, roadPrefabs.Length)); 
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SpawnRoad(int RoadIndex)
+    {
+
+        Debug.Log("KABOM!!!!"); 
+        Instantiate(roadPrefabs[RoadIndex], transform.forward * zspawn, transform.rotation);
+        zspawn = roadLength;
+    
+    }
+}
