106229ad96c3ee0eef7a07fa397c2a64697bbc09
Or Abuhtzura <44296674+orabu103@users.noreply.github.com>
2020-05-26 14:14:58 +0300

    Add car mover

---
+++ Assignment 9/solutions/yourgame/CanYouDrive1/Assets/Scripts/Mover.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+    public class Mover : MonoBehaviour
+    {
+        [Tooltip("Movement vector in meters per second")]
+        [SerializeField] Vector3 velocity;
+        public int speed = 20;
+
+        void Update()
+        {
+            transform.position += velocity * Time.deltaTime * speed;
+        }
+
+    }
